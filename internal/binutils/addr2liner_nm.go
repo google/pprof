ab2ad3d69d2231aaa937f4aaa693a9bfb18eb900
Raul Silvera <rsilvera@google.com>
2016-03-04 08:16:37 -0800

    Get rid of newlines at the end of function names from nm symbolization
    They were thrashing pprof's output

--- internal/binutils/addr2liner_nm.go
+++ internal/binutils/addr2liner_nm.go
@@ -61,7 +61,7 @@ func newAddr2LinerNM(cmd, file string, base uint64) (*addr2LinerNM, error) {
 		return nil, err
 	}
 
-	// Parse addr2line output and populate symbol map.
+	// Parse nm output and populate symbol map.
 	// Skip lines we fail to parse.
 	buf := bufio.NewReader(&b)
 	for {
@@ -72,6 +72,7 @@ func newAddr2LinerNM(cmd, file string, base uint64) (*addr2LinerNM, error) {
 			}
 			return nil, err
 		}
+		line = strings.TrimSpace(line)
 		fields := strings.SplitN(line, " ", 3)
 		if len(fields) != 3 {
 			continue
