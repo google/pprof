74cc49f9f0415cac90cf6cc362eee6208f1753b9
Raul Silvera <rsilvera@google.com>
2017-03-10 09:37:17 -0800

    Remove filenames when showing function (default) granularity
    Previously we were showing filenames together with function names
    when generating reports at function granularity. This was an attempt
    to provide more information to the user, but it clutters the output.
    Also, in cases where a function is associated to multiple files
    (eg template instantiations in C++), keeping the filename may prevent
    us from combining nodes from the same function.
    
    Will stop keeping filenames at the default function granularity, and will
    remove the "functionnameonly" option which was previously providing this
    functionality.

--- internal/driver/commands.go
+++ internal/driver/commands.go
@@ -227,9 +227,6 @@ var pprofVariables = variables{
 	"functions": &variable{boolKind, "t", "granularity", helpText(
 		"Aggregate at the function level.",
 		"Takes into account the filename/lineno where the function was defined.")},
-	"functionnameonly": &variable{boolKind, "f", "granularity", helpText(
-		"Aggregate at the function level.",
-		"Ignores the filename/lineno where the function was defined.")},
 	"files": &variable{boolKind, "f", "granularity", "Aggregate at the file level."},
 	"lines": &variable{boolKind, "f", "granularity", "Aggregate at the source code line level."},
 	"addresses": &variable{boolKind, "f", "granularity", helpText(
