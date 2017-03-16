d0fc29995ff5c61e7049146d3fe4a9f49749cc1d
Alexey Alexandrov <aalexand@google.com>
2017-03-16 09:25:17 -0700

    Push testing kind of commands down to test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,13 @@
 set -e
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v vendor); do
+PKG=$(go list ./... | grep -v /vendor/)
+
+staticcheck $PKG
+unused $PKG
+go test -v $PKG
+
+for d in $PKG; do
   go test -race -coverprofile=profile.out -covermode=atomic $d
   if [ -f profile.out ]; then
     cat profile.out >> coverage.txt
