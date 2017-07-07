3f5db817b46c7dd7e43614f3a701b21086bd050f
Maggie Nolan <nolanmar@google.com>
2017-07-06 17:30:21 -0700

    Fixed formatting error in fuzz/main.go

--- fuzz/main.go
+++ fuzz/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/google/pprof/profile"
 )
 
+// Fuzz can be used with https://github.com/dvyukov/go-fuzz to do fuzz testing on ParseData
 func Fuzz(data []byte) int {
 	profile.ParseData(data)
 	return 0
