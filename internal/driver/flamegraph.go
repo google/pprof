4a033973f1f3b213b88042c3bd611e3bca917173
Martin Spier <spiermar@gmail.com>
2017-09-25 20:51:06 -0700

    chore: copyright update

--- internal/driver/flamegraph.go
+++ internal/driver/flamegraph.go
@@ -1,4 +1,4 @@
-// Copyright © 2017 Martin Spier <spiermar@gmail.com>
+// Copyright 2017 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
