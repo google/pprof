google/pprof

commit f0154354e2b55b30901c53a1b6ac81c710c2aff4

Allow passing in a custom HTTPServer (#341)
* Avoid opening browser when not in terminal

This also reverts e82ee9a, though only in spirit since the revert
did not apply cleanly.

This change may look cosmetic, but note that `plugin.UI` can be
passed in from the outside. This is important for embedding pprof
into an application for easy access to the web ui: with this change,
such an application can pass a non-Terminal `profile.UI` and avoids
the browser code.

* Allow passing in a custom HTTPServer

This allows embedding the pprof ui into an existing application.
See https://github.com/cockroachdb/cockroach/pull/24145 for an example.

driver/driver.go
@@ -41,24 +41,36 @@ func (o *Options) internalOptions() *plugin.Options {
 	if o.Sym != nil {
 		sym = &internalSymbolizer{o.Sym}
 	}
+	var httpServer func(args *plugin.HTTPServerArgs) error
+	if o.HTTPServer != nil {
+		httpServer = func(args *plugin.HTTPServerArgs) error {
+			return o.HTTPServer(((*HTTPServerArgs)(args)))
+		}
+	}
 	return &plugin.Options{
-		Writer:  o.Writer,
-		Flagset: o.Flagset,
-		Fetch:   o.Fetch,
-		Sym:     sym,
-		Obj:     obj,
-		UI:      o.UI,
+		Writer:     o.Writer,
+		Flagset:    o.Flagset,
+		Fetch:      o.Fetch,
+		Sym:        sym,
+		Obj:        obj,
+		UI:         o.UI,
+		HTTPServer: httpServer,
 	}
 }
 
+// HTTPServerArgs contains arguments needed by an HTTP server that
+// is exporting a pprof web interface.
+type HTTPServerArgs plugin.HTTPServerArgs
+
 // Options groups all the optional plugins into pprof.
 type Options struct {
-	Writer  Writer
-	Flagset FlagSet
-	Fetch   Fetcher
-	Sym     Symbolizer
-	Obj     ObjTool
-	UI      UI
+	Writer     Writer
+	Flagset    FlagSet
+	Fetch      Fetcher
+	Sym        Symbolizer
+	Obj        ObjTool
+	UI         UI
+	HTTPServer func(*HTTPServerArgs) error
 }
 
 // Writer provides a mechanism to write data under a certain name,

internal/driver/webui.go
@@ -82,7 +82,7 @@ type webArgs struct {
 	FlameGraph template.JS
 }
 
-func serveWebInterface(hostport string, p *profile.Profile, o *plugin.Options, wantBrowser bool) error {
+func serveWebInterface(hostport string, p *profile.Profile, o *plugin.Options) error {
 	host, port, err := getHostAndPort(hostport)
 	if err != nil {
 		return err
@@ -117,7 +117,7 @@ func serveWebInterface(hostport string, p *profile.Profile, o *plugin.Options, w
 		},
 	}
 
-	if wantBrowser {
+	if o.UI.IsTerminal() {
 		go openBrowser("http://"+args.Hostport, o)
 	}
 	return server(args)

internal/driver/webui_test.go
@@ -31,6 +31,12 @@ import (
 	"github.com/google/pprof/profile"
 )
 
+type nonTerminalUI struct {
+	plugin.UI
+}
+
+func (*nonTerminalUI) IsTerminal() bool { return false }
+
 func TestWebInterface(t *testing.T) {
 	if runtime.GOOS == "nacl" {
 		t.Skip("test assumes tcp available")
@@ -55,9 +61,9 @@ func TestWebInterface(t *testing.T) {
 	// Start server and wait for it to be initialized
 	go serveWebInterface("unused:1234", prof, &plugin.Options{
 		Obj:        fakeObjTool{},
-		UI:         &stdUI{},
+		UI:         &nonTerminalUI{&stdUI{}}, // don't open browser
 		HTTPServer: creator,
-	}, false)
+	})
 	<-serverCreated
 	defer server.Close()
 

internal/driver/driver.go
@@ -54,7 +54,7 @@ func PProf(eo *plugin.Options) error {
 	}
 
 	if src.HTTPHostport != "" {
-		return serveWebInterface(src.HTTPHostport, p, o, true)
+		return serveWebInterface(src.HTTPHostport, p, o)
 	}
 	return interactive(p, o)
 }

commit 00bb1c3a0fac322bdb1658fd8d10b1fef2e6cd2b

Introduce (UI).WantBrowser
This fixes a bug introduced in #341 which caused the browser
opening code to be disabled for the standard `pprof -http`
invocation.

Introduce a proper getter on the UI which determines whether
the browser ought to be opened.

driver/driver.go
@@ -218,6 +218,9 @@ type UI interface {
 	// interactive terminal (as opposed to being redirected to a file).
 	IsTerminal() bool
 
+	// WantBrowser indicates whether browser should be opened with the -http option.
+	WantBrowser() bool
+
 	// SetAutoComplete instructs the UI to call complete(cmd) to obtain
 	// the auto-completion of cmd, if the UI supports auto-completion at all.
 	SetAutoComplete(complete func(string) string)

internal/driver/options.go
@@ -128,6 +128,10 @@ func (ui *stdUI) IsTerminal() bool {
 	return false
 }
 
+func (ui *stdUI) WantBrowser() bool {
+	return true
+}
+
 func (ui *stdUI) SetAutoComplete(func(string) string) {
 }
 

internal/driver/webui.go
@@ -117,7 +117,7 @@ func serveWebInterface(hostport string, p *profile.Profile, o *plugin.Options) e
 		},
 	}
 
-	if o.UI.IsTerminal() {
+	if o.UI.WantBrowser() {
 		go openBrowser("http://"+args.Hostport, o)
 	}
 	return server(args)

internal/plugin/plugin.go
@@ -192,6 +192,9 @@ type UI interface {
 	// interactive terminal (as opposed to being redirected to a file).
 	IsTerminal() bool
 
+	// WantBrowser indicates whether a browser should be opened with the -http option.
+	WantBrowser() bool
+
 	// SetAutoComplete instructs the UI to call complete(cmd) to obtain
 	// the auto-completion of cmd, if the UI supports auto-completion at all.
 	SetAutoComplete(complete func(string) string)

internal/proftest/proftest.go
@@ -130,6 +130,11 @@ func (ui *TestUI) IsTerminal() bool {
 	return false
 }
 
+// WantBrowser indicates whether a browser should be opened with the -http option.
+func (ui *TestUI) WantBrowser() bool {
+	return false
+}
+
 // SetAutoComplete is not supported by the test UI.
 func (ui *TestUI) SetAutoComplete(_ func(string) string) {
 }

internal/driver/webui_test.go
@@ -28,14 +28,9 @@ import (
 	"testing"
 
 	"github.com/google/pprof/internal/plugin"
+	"github.com/google/pprof/internal/proftest"
 	"github.com/google/pprof/profile"
 )
-
-type nonTerminalUI struct {
-	plugin.UI
-}
-
-func (*nonTerminalUI) IsTerminal() bool { return false }
 
 func TestWebInterface(t *testing.T) {
 	if runtime.GOOS == "nacl" {
@@ -61,7 +56,7 @@ func TestWebInterface(t *testing.T) {
 	// Start server and wait for it to be initialized
 	go serveWebInterface("unused:1234", prof, &plugin.Options{
 		Obj:        fakeObjTool{},
-		UI:         &nonTerminalUI{&stdUI{}}, // don't open browser
+		UI:         &proftest.TestUI{},
 		HTTPServer: creator,
 	})
 	<-serverCreated