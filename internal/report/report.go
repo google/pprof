6b6c5363f387cfbce7a6a5dbaa81618efbe3e874
Maggie Nolan <nolanmar@google.com>
2018-04-26 16:36:42 -0700

    remove -positive_percentages flag

--- internal/report/report.go
+++ internal/report/report.go
@@ -55,16 +55,15 @@ const (
 type Options struct {
 	OutputFormat int
 
-	CumSort             bool
-	CallTree            bool
-	DropNegative        bool
-	PositivePercentages bool
-	CompactLabels       bool
-	Ratio               float64
-	Title               string
-	ProfileLabels       []string
-	ActiveFilters       []string
-	NumLabelUnits       map[string]string
+	CumSort       bool
+	CallTree      bool
+	DropNegative  bool
+	CompactLabels bool
+	Ratio         float64
+	Title         string
+	ProfileLabels []string
+	ActiveFilters []string
+	NumLabelUnits map[string]string
 
 	NodeCount    int
 	NodeFraction float64
@@ -1192,7 +1191,7 @@ func New(prof *profile.Profile, o *Options) *Report {
 		}
 		return measurement.ScaledLabel(v, o.SampleUnit, o.OutputUnit)
 	}
-	return &Report{prof, computeTotal(prof, o.SampleValue, o.SampleMeanDivisor, !o.PositivePercentages),
+	return &Report{prof, computeTotal(prof, o.SampleValue, o.SampleMeanDivisor),
 		o, format}
 }
 
@@ -1213,11 +1212,8 @@ func NewDefault(prof *profile.Profile, options Options) *Report {
 }
 
 // computeTotal computes the sum of all sample values. This will be
-// used to compute percentages. If includeNegative is set, use use
-// absolute values to provide a meaningful percentage for both
-// negative and positive values. Otherwise only use positive values,
-// which is useful when comparing profiles from different jobs.
-func computeTotal(prof *profile.Profile, value, meanDiv func(v []int64) int64, includeNegative bool) int64 {
+// used to compute percentages.
+func computeTotal(prof *profile.Profile, value, meanDiv func(v []int64) int64) int64 {
 	var div, ret int64
 	for _, sample := range prof.Sample {
 		var d, v int64
@@ -1225,13 +1221,11 @@ func computeTotal(prof *profile.Profile, value, meanDiv func(v []int64) int64, i
 		if meanDiv != nil {
 			d = meanDiv(sample.Value)
 		}
-		if v >= 0 {
-			ret += v
-			div += d
-		} else if includeNegative {
-			ret -= v
-			div += d
+		if v < 0 {
+			v = -v
 		}
+		ret += v
+		div += d
 	}
 	if div != 0 {
 		return ret / div
