50fd8f7be0a4647652ac7f1ec021f180fbf4145e
Kalyana Chadalavada <kalyanac@users.noreply.github.com>
2019-02-06 23:28:42 -0800

    Update build_binaries.sh

--- internal/binutils/testdata/build_binaries.sh
+++ internal/binutils/testdata/build_binaries.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -x
+
 # Copyright 2019 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/bin/bash -x
 
 # This is a script that generates the test executables for MacOS and Linux
 # in this directory. It should be needed very rarely to run this script.
