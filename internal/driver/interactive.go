68e3bcf5a3799b7ef153aa9c592593c292a58937
Garrett Wang <garrettwang@google.com>
2020-04-09 17:17:04 -0700

    Rename the function from addVariablesPostFix to variablesPostFix

--- internal/driver/interactive.go
+++ internal/driver/interactive.go
@@ -72,7 +72,7 @@ func interactive(p *profile.Profile, o *plugin.Options) error {
 				if v := pprofVariables[name]; v != nil {
 					// All non-bool options require inputs
 					if v.kind != boolKind && value == "" {
-						example := name + addVariablesPostFix(name, v)
+						example := name + variablesPostFix(name, v)
 						o.UI.PrintErr(fmt.Errorf("please input a value, e.g. %v", example))
 						continue
 					}
