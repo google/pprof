8353b3c27a7d4805d419c93d2bd9065c159f4772
Oleg Kovalov <oleg@hey.com>
2023-09-01 18:03:03 +0200

    review suggestions

--- third_party/d3flamegraph/update.sh
+++ third_party/d3flamegraph/update.sh
@@ -11,20 +11,37 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-
 #!/usr/bin/env bash
-
 set -eu
 set -o pipefail
-
 D3FLAMEGRAPH_CSS="d3-flamegraph.css"
 
 cd $(dirname $0)
 
-install_and_copy() {
+generate_d3_flame_graph_go() {
     npm install
+    # https://stackoverflow.com/a/21199041/171898
+    local d3_js=$(cat d3.js | sed 's/`/`+"`"+`/g')
+    local d3_css=$(cat "node_modules/d3-flame-graph/dist/${D3FLAMEGRAPH_CSS}")
 
-    cp node_modules/d3-flame-graph/dist/${D3FLAMEGRAPH_CSS} d3.css
+    cat <<-EOF > d3_flame_graph.go
+// D3.js is a JavaScript library for manipulating documents based on data.
+// https://github.com/d3/d3
+// See D3_LICENSE file for license details
+// d3-flame-graph is a D3.js plugin that produces flame graphs from hierarchical data.
+// https://github.com/spiermar/d3-flame-graph
+// See D3_FLAME_GRAPH_LICENSE file for license details
+package d3flamegraph
+// JSSource returns the d3 and d3-flame-graph JavaScript bundle
+const JSSource = \`
+$d3_js
+\`
+// CSSSource returns the $D3FLAMEGRAPH_CSS file
+const CSSSource = \`
+$d3_css
+\`
+EOF
+    gofmt -w d3_flame_graph.go
 }
 
 get_licenses() {
@@ -33,4 +50,4 @@ get_licenses() {
 }
 
 get_licenses
-install_and_copy
+generate_d3_flame_graph_go
