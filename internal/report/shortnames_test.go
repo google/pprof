d43a673792989bf1cc768f264733434c97877ff4
Sanjay Ghemawat <sanjay@google.com>
2024-09-27 11:03:34 -0700

    Support different display granularities in flame graph view. (#896)
    * Support different display granularities in flame graph view.
    
    Previously, flame-graph view could only display filefunctions
    granularity. We can now support all the available granularities.
    
    This allows the user to supply a granularity either via a command-line
    flag (e.g., --files), or as a URL parameter (e.g., ?g=files).
    
    Details
    * Made the default initial granularity "" so that we can stick with
      the default flame-graph granularity of "filefunctions" while also
      allowing overrides.
    * Use different computation of the list of shorter display names
      for file names (strip off leading path components instead of package
      prefixes).
    * Similarly compute color based on directory name instead of package name.
    * Include line number and column number in displayed names if the
      granularity includes line numbers.
    
    * Improve granularity entry for options command
    
    ---------
    
    Co-authored-by: Alexey Alexandrov <aalexand@users.noreply.github.com>

--- internal/report/shortnames_test.go
+++ internal/report/shortnames_test.go
@@ -32,3 +32,29 @@ func TestShortNames(t *testing.T) {
 		})
 	}
 }
+
+func TestFileNameSuffixes(t *testing.T) {
+	type testCase struct {
+		name string
+		in   string
+		out  []string
+	}
+	test := func(name, in string, out ...string) testCase {
+		return testCase{name, in, out}
+	}
+
+	for _, c := range []testCase{
+		test("empty", "", "."),
+		test("simple", "foo", "foo"),
+		test("manypaths", "a/b/c", "a/b/c", "b/c", "c"),
+		test("leading", "/a/b", "/a/b", "a/b", "b"),
+		test("trailing", "a/b", "a/b", "b"),
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			got := fileNameSuffixes(c.in)
+			if !reflect.DeepEqual(c.out, got) {
+				t.Errorf("fileNameSuffixes(%q) = %#v, expecting %#v", c.in, got, c.out)
+			}
+		})
+	}
+}
