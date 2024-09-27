d3c342702e3c97045bb7013a76dec97e9b0464f9
Sanjay Ghemawat <sanjay@google.com>
2024-09-27 13:50:24 -0700

    Report missing file name as "", not "."

--- internal/report/shortnames.go
+++ internal/report/shortnames.go
@@ -29,6 +29,10 @@ var (
 // fileNameSuffixes returns a non-empty sequence of shortened file names
 // (in decreasing preference) that can be used to represent name.
 func fileNameSuffixes(name string) []string {
+	if name == "" {
+		// Avoid returning "." when symbol info is missing
+		return []string{""}
+	}
 	return allSuffixes(filepath.ToSlash(filepath.Clean(name)), fileSepRE)
 }
 
