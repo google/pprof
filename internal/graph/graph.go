ceb00519c15302caeb4a2ea19dd5223059a1e4fc
1911860538 <alxps1911@gmail.com>
2025-03-15 11:12:29 +0800

    Replace for-loop key deletion with clear in newGraph function

--- internal/graph/graph.go
+++ internal/graph/graph.go
@@ -336,12 +336,8 @@ func newGraph(prof *profile.Profile, o *Options) (*Graph, map[uint64]Nodes) {
 		if dw == 0 && w == 0 {
 			continue
 		}
-		for k := range seenNode {
-			delete(seenNode, k)
-		}
-		for k := range seenEdge {
-			delete(seenEdge, k)
-		}
+		clear(seenNode)
+		clear(seenEdge)
 		var parent *Node
 		// A residual edge goes over one or more nodes that were not kept.
 		residual := false
