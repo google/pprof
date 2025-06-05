50936fd52aa3b31fd3059f311885ad3824efa1d0
cuishuang <imcusg@gmail.com>
2025-06-05 12:09:51 +0800

    refactor: use a more modern writing style
    Signed-off-by: cuishuang <imcusg@gmail.com>

--- profile/profile.go
+++ profile/profile.go
@@ -24,6 +24,7 @@ import (
 	"math"
 	"path/filepath"
 	"regexp"
+	"slices"
 	"sort"
 	"strings"
 	"sync"
@@ -734,12 +735,7 @@ func (p *Profile) RemoveLabel(key string) {
 
 // HasLabel returns true if a sample has a label with indicated key and value.
 func (s *Sample) HasLabel(key, value string) bool {
-	for _, v := range s.Label[key] {
-		if v == value {
-			return true
-		}
-	}
-	return false
+	return slices.Contains(s.Label[key], value)
 }
 
 // SetNumLabel sets the specified key to the specified value for all samples in the
